4c6d96c6f6c0907b0867d1365c1ed6e7c3c41b50
kauan mello teixeira <111128017+madebykauan@users.noreply.github.com>
2024-08-30 12:18:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function verificarDia() {
-    var dia = document.getElementById('dia').value.toLowerCase().trim();
-    var respostaDia = document.getElementById('respostaDia');
-    var diasUteis = ["segunda", "terça", "terca", "quarta", "quinta", "sexta"];
-    var fimDeSemana = ["sábado", "sabado", "domingo"];
 
-    if (diasUteis.includes(dia)) {
-        respostaDia.innerHTML = "É dia útil.";
-    } else if (fimDeSemana.includes(dia)) {
-        respostaDia.innerHTML = "É fim de semana.";
-    } else {
-        respostaDia.innerHTML = "Dia inválido. Por favor, insira um dia da semana válido.";
-    }
-}
